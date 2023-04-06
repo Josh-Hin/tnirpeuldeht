d08fa893a281151ba5c15034bdb666c2362470ee
Josh-Hin <110961976+Josh-Hin@users.noreply.github.com>
2023-04-06 14:26:52 +1000

    updates

--- js/extra.js
+++ js/extra.js
@@ -7,7 +7,7 @@
         $(this).contents().find("div[class='md-main__inner md-grid']").css("margin-top", "0px");
         $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("margin", "0 0 0 0");
         $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("font-weight", "300");
-        $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("font-size", "2em");
+        $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("font-size", "1.9em");
         $("#loading-news-1").css("display", "none");
         $(this).show();
         
@@ -21,7 +21,7 @@
         $(this).contents().find("div[class='md-main__inner md-grid']").css("margin-top", "0px");
         $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("margin", "0 0 0 0");
         $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("font-weight", "300");
-        $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("font-size", "2em");
+        $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("font-size", "1.9em");
         $("#loading-news-2").css("display", "none");
         $(this).show();
     });
@@ -34,7 +34,7 @@
         $(this).contents().find("div[class='md-main__inner md-grid']").css("margin-top", "0px");
         $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("margin", "0 0 0 0");
         $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("font-weight", "300");
-        $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("font-size", "2em");
+        $(this).contents().find("article[class='md-content__inner md-typeset'] h1").css("font-size", "1.9em");
         $("#loading-news-3").css("display", "none");
         $(this).show();
     });
@@ -56,6 +56,61 @@
         $("#cs-span").fadeIn(4000);
     });
 
-
+    // Last Modified
+    function readDate() {
+        let mod_date = document.lastModified;
+        var date = new Date(mod_date);
+        var day = date.getDate();
+        var year = date.getFullYear();
+        var month = date.getMonth();
+        switch(month) {
+            case 0: m = "January";
+                break;   
+            case 1: m = "February";
+                break; 
+            case 2: m = "March";
+                break;   
+            case 3: m = "April";
+                break;  
+            case 4: m = "May";
+                break;   
+            case 5: m = "June";
+                break; 
+            case 6: m = "July";
+                break;   
+            case 7: m = "August";
+                break; 
+            case 8: m = "September";
+                break;   
+            case 9: m = "October";
+                break; 
+            case 10: m = "November";
+                break;   
+            case 11: m = "December";
+                break;             
+        }
+        let text = day + " " + m + " " + year;
+        var lastMod = document.getElementById("lastModified");
+        if (lastMod) {
+            lastMod.innerHTML = text;
+        }
+    }
+    readDate();
+      
+    // Calculate Reading Time 
+    function readTime() {
+        const text = document.querySelector("article").innerText;
+        const wpm = 200;
+        const words = text.trim().split(/\s+/).length;
+        const time = Math.ceil(words / wpm);
+        var rtime = document.getElementById("time");
+        if (rtime) {
+            rtime.innerText = time;
+        }
+    }
+    readTime();
+     
+    /** Place HTML in Markdown to generate Date and Time **/
+    // <p id="date-and-time"><span id="time"></span> minutes to read - <span id="lastModified"></span></p> 
 
    
